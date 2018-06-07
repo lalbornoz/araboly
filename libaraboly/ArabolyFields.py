b476e10ff8ab11ac0e82eb581e590f01e3291c1a
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2018-06-07 12:19:01 +0200

    libaraboly/ArabolyFields.py:_land_property_utility(): print field landing strings at normal output level (e.g. delayed.)

--- libaraboly/ArabolyFields.py
+++ libaraboly/ArabolyFields.py
@@ -110,7 +110,7 @@ class ArabolyFields(ArabolyTypeClass):
                     srcPropRent *= 2
                 for rentString in srcField["strings"][ArabolyStringType.LAND][srcField["level"]]:
                     rands = [ArabolyRandom(limit=150-5, min=5) for x in range(10)]
-                    output = ArabolyGenerals._push_output(channel, context, output, rentString.format(cost=srcPropRent, owner=srcField["owner"], prop=srcField["title"], rands=rands, who=src), ArabolyOutputLevel.LEVEL_NODELAY)
+                    output = ArabolyGenerals._push_output(channel, context, output, rentString.format(cost=srcPropRent, owner=srcField["owner"], prop=srcField["title"], rands=rands, who=src))
                 context.players["byName"][srcField["owner"]]["wallet"] += srcPropRent
                 srcPlayer["wallet"] -= srcPropRent
         return context, output, srcField, srcPlayer
