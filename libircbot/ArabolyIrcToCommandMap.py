5ea287aec72dbc4c7412b34476d30d2dc8a03991
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2018-06-09 18:50:03 +0200

    ArabolyDebugger.py, assets/text/ArabolyDebuggerUsage.txt: implements [-o <snapshot pathname>] (converts savefile to snapshot.) assets/text/README.md, assets/MiRCart/ArabolyIrcBot.hlp, libircbot/ArabolyIrcToCommandMap.py: updated. libaraboly/ArabolyAttractMode.py:dispatch_load(): [l]oad local snapshot from <pathname>. libaraboly/ArabolyFree.py:dispatch_status(): correctly determine if statusPlayer exists.

--- libircbot/ArabolyIrcToCommandMap.py
+++ libircbot/ArabolyIrcToCommandMap.py
@@ -17,8 +17,8 @@ class ArabolyIrcToCommandMap(ArabolyTypeClass):
     """XXX"""
     commandsFilter = [
         "accept", "bid", "board", "buy", "cheat", "develop", "dice",
-        "help", "join", "kick", "lift", "melp", "mortgage", "part",
-        "pass", "reject", "sell", "start", "status", "stop"]
+        "help", "join", "kick", "lift", "load", "melp", "mortgage",
+        "part", "pass", "reject", "sell", "start", "status", "stop"]
 
     # {{{ dispatch001(context, output): Dispatch single 001 (RPL_WELCOME)
     @staticmethod
